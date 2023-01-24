4ff105fde93feea3fd773d75f764c9cf0a6b646e
apoznanski <71227648+apoznanski@users.noreply.github.com>
2023-01-24 12:02:12 -0500

    translated src/pagination.js to ts

--- src/pagination.ts
+++ src/pagination.ts
@@ -1,11 +1,9 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-const querystring_1 = __importDefault(require("querystring"));
-const lodash_1 = __importDefault(require("lodash"));
-function createPagination(currentPage, pageCount, queryObj) {
+import qs from 'querystring';
+import _ from 'lodash';
+
+export default function createPagination(currentPage: number, pageCount: number, queryObj: {
+    page: number,
+    active: boolean }) {
     if (pageCount <= 1) {
         return {
             prev: { page: 1, active: currentPage > 1 },
@@ -18,36 +16,41 @@ function createPagination(currentPage, pageCount, queryObj) {
             pageCount: 1,
         };
     }
+
     let pagesToShow = [1, 2, pageCount - 1, pageCount];
+
     currentPage = currentPage || 1;
     const previous = Math.max(1, currentPage - 1);
     const next = Math.min(pageCount, currentPage + 1);
+
     let startPage = Math.max(1, currentPage - 2);
     if (startPage > pageCount - 5) {
         startPage -= 2 - (pageCount - currentPage);
     }
-    let i;
+    let i: number;
     for (i = 0; i < 5; i += 1) {
         pagesToShow.push(startPage + i);
     }
-    pagesToShow = lodash_1.default.uniq(pagesToShow).filter(page => page > 0 && page <= pageCount).sort((a, b) => a - b);
+
+    pagesToShow = _.uniq(pagesToShow).filter(page => page > 0 && page <= pageCount).sort((a, b) => a - b);
+
     const pages = pagesToShow.map((page) => {
         queryObj.page = page;
-        return { page: page, active: page === currentPage, qs: querystring_1.default.stringify(queryObj), separator: false };
+        return { page: page, active: page === currentPage, qs: qs.stringify(queryObj), separator: false };
     });
     for (i = pages.length - 1; i > 0; i -= 1) {
         if (pages[i].page - 2 === pages[i - 1].page) {
             pages.splice(i, 0, {
                 page: pages[i].page - 1,
                 active: false,
-                qs: querystring_1.default.stringify(queryObj),
+                qs: qs.stringify(queryObj),
                 separator: false,
             });
-        }
-        else if (pages[i].page - 1 !== pages[i - 1].page) {
-            pages.splice(i, 0, { page: pages[i].page - 1, active: false, qs: querystring_1.default.stringify(queryObj), separator: true });
+        } else if (pages[i].page - 1 !== pages[i - 1].page) {
+            pages.splice(i, 0, { page: pages[i].page - 1, active: false, qs: qs.stringify(queryObj), separator: true });
         }
     }
+
     const data = {
         rel: [],
         pages: pages,
@@ -59,25 +62,27 @@ function createPagination(currentPage, pageCount, queryObj) {
         last: { page: 1, active: currentPage === pageCount, qs: '' },
     };
     queryObj.page = previous;
-    data.prev = { page: previous, active: currentPage > 1, qs: querystring_1.default.stringify(queryObj) };
+    data.prev = { page: previous, active: currentPage > 1, qs: qs.stringify(queryObj) };
     queryObj.page = next;
-    data.next = { page: next, active: currentPage < pageCount, qs: querystring_1.default.stringify(queryObj) };
+    data.next = { page: next, active: currentPage < pageCount, qs: qs.stringify(queryObj) };
+
     queryObj.page = 1;
-    data.first = { page: 1, active: currentPage === 1, qs: querystring_1.default.stringify(queryObj) };
+    data.first = { page: 1, active: currentPage === 1, qs: qs.stringify(queryObj) };
     queryObj.page = pageCount;
-    data.last = { page: pageCount, active: currentPage === pageCount, qs: querystring_1.default.stringify(queryObj) };
+    data.last = { page: pageCount, active: currentPage === pageCount, qs: qs.stringify(queryObj) };
+
     if (currentPage < pageCount) {
         data.rel.push({
             rel: 'next',
-            href: `?${querystring_1.default.stringify(Object.assign(Object.assign({}, queryObj), { page: next }))}`,
+            href: `?${qs.stringify({ ...queryObj, page: next })}`,
         });
     }
+
     if (currentPage > 1) {
         data.rel.push({
             rel: 'prev',
-            href: `?${querystring_1.default.stringify(Object.assign(Object.assign({}, queryObj), { page: previous }))}`,
+            href: `?${qs.stringify({ ...queryObj, page: previous })}`,
         });
     }
     return data;
 }
-exports.default = createPagination;
